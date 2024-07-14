a7585e1ff68ad1382e2d1481cb7dbbaad8053e5b
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-07-14 23:38:08 +1000

    handle all non-api get requests

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use('/', routes)
 
 app.use(express.static(path.resolve(__dirname, './client/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
