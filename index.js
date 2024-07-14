7999fb8b9d5bbeb77c0c8db0000795d3ad0d446d
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-07-14 14:41:41 +1000

    update port number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 // const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 
 // Middleware to parse JSON bodies
